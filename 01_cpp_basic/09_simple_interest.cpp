26719407c0acc9a74101f3e813d79a03752cb703
amit <amitprajapati5659@gmail.com>
2026-08-22 12:41:57 +0530

    Modified

--- 01_cpp_basic/09_simple_interest.cpp
+++ 01_cpp_basic/09_simple_interest.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main(){
 
+    float principle_amt = 5000;
+    float time_duration = 5;
+    float interest_rate = 6;
 
-    
+    float simple_interest = (principle_amt * time_duration * interest_rate) / 100;
+    cout << "Simple interest: " << simple_interest << "\n";
+    cout << "Total amount to pay :" << simple_interest + principle_amt;
+
     return 0;
 
 }

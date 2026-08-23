e413f0c763eaa0cd38c77dcf57756acdcf2943a3
amit <amitprajapati5659@gmail.com>
2026-08-23 16:35:35 +0530

    Modified

--- 02_operator/04_arithmetic.cpp
+++ 02_operator/04_arithmetic.cpp
@@ -5,9 +5,9 @@ int main(){
     int a = 4 ;
     int b = 5;
     cout << "Addition : " << (a + b) << "\n";
-    cout << "Addition : " << (a - b) << "\n";
-    cout << "Addition : " << (a * b) << "\n";
-    cout << "Addition : " << (a / b ) << "\n";
-    cout << "Addition : " << (a % b ) << "\n";
+    cout << "Subtraction : " << (a - b) << "\n";
+    cout << "Multiplication : " << (a * b) << "\n";
+    cout << "Division : " << (a / b ) << "\n";
+    cout << "Modulus : " << (a % b ) << "\n";
     return 0;
 }

a18b7201616e7d6288e1720945fb211286e1f5bb
amit <amitprajapati5659@gmail.com>
2026-08-24 11:02:42 +0530

    Prime number checker

---
+++ 04_loops/12_prime_number.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    bool isPrime = true;
+    if (num == 1 || num == 2)
+    {
+        cout << num << " is a prime number." << endl;
+        return 0;
+    }
+
+    for (int i = 2; i < num; ++i)
+    {
+        if (num % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+    {
+        cout << num << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << num << " is not a prime number." << endl;
+    }
+    return 0;
+}
---
+++ 04_loops/12_prime_number_optimized.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    bool isPrime = true;
+    if (num == 1 || num == 2)
+    {
+        cout << num << " is a prime number." << endl;
+        return 0;
+    }
+
+    for (int i = 2; i <= sqrt(num); ++i)
+    {
+        if (num % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+    {
+        cout << num << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << num << " is not a prime number." << endl;
+    }
+    return 0;
+}
